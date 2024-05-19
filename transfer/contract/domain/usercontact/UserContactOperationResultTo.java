fe4070ddef7be34c17016f966a6b6963f67100dc
Vyach12 <slava.gusarov.73@mail.ru>
2024-05-19 15:36:26 +0300

    [feat]: initialized gitsubmodule correctly

---
+++ transfer/contract/domain/usercontact/UserContactOperationResultTo.java
@@ -0,0 +1,11 @@
+package transfer.contract.domain.usercontact;
+
+import lombok.Builder;
+
+/**
+ * Результат операции над контактом пользователя.
+ */
+@Builder(toBuilder = true)
+public class UserContactOperationResultTo {
+
+}
---
+++ transfer/contract/domain/usercontact/UserContactTo.java
@@ -0,0 +1,16 @@
+package transfer.contract.domain.usercontact;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+/**
+ * To-класс для пользовательского контакта.
+ *
+ * @param id идентификатор контакта
+ * @param user1Id идентификатор 1 пользователя
+ * @param user2Id идентификатор 2 пользователя
+ */
+@Builder(toBuilder = true)
+public record UserContactTo(UUID id, UUID user1Id, UUID user2Id) {
+}
